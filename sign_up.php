f8e3327319abe8ff8f0ba9c57cc5c3bf3214bf20
Jonathan Castillo Hurtado <jcastillo@xxi-banorte.com>
2024-04-25 17:30:11 -0600

    create and delegate navbar

--- sign_up.php
+++ sign_up.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<title>Unete</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <iframe src="UI/navbar.html" class ="w-100" seamless></iframe>
+	  <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include "UI/navbar.html" ?>
 
 </head>
 <body>
